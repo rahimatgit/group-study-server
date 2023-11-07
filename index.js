11207a4cc7c3fd9ea4a9d7066e5794e7a4591323
rahimatgit <rahimmonjur9@gmail.com>
2023-11-07 12:20:49 -0800

    mongodb setup and all assignment data loading api completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,6 +15,47 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bgmtqy5.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const assignmentCollection = client.db('groupStudy').collection('assignments');
+
+
+    // all assignment
+    app.get('/assignments', async(req, res) => {
+        const cursor = assignmentCollection.find();
+            const result = await cursor.toArray();
+            res.send(result); 
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('assignMate is running')
